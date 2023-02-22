3e5c74d18f99d0bb41a4d6ba7a470974643ed991
Aarne Andersson <n0anaa00@students.oamk.fi>
2023-02-22 02:07:07 +0200

    viimeinen versio

--- App.js
+++ App.js
@@ -1,20 +1,133 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TouchableOpacity, View, Text,  View } from 'react-native';
+import { ScrollView } from 'react-native-web';
+import {NumericInput} from 'react-native-numeric-input';
+import {RadioButton} from 'react-native-paper';
+import { TextInput } from 'react-native';
+import { Styles } from './styles/Styles';
+import { useState } from 'react';
 
 export default function App() {
+ 
+  
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [result, setResult] = useState(0);
+  
+  const [radioval, setRadioval] = useState('male');
+  const radios = [
+    {val: 'male', label: 'male'},
+    {val: 'female', label: 'female'},
+    
+  ]
+  
+
+  
+
+
+  function calculateBAL(e) {
+    e.preventDefault();
+    const calcLitres = 0.33 * bottles;
+    const calcGrams = calcLitres * 8 * 4.5;
+    const calcBurning = weight / 10;
+    const calfGramsleft = calcGrams - calcBurning * time;
+
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+
+    if (male.checked==true){
+      setResult(calfGramsleft / (weight * 0.7));
+    } else if(female.checked==true){
+      setResult(calfGramsleft / (weight * 0.6));
+    }
+
+    if (weight==null){
+      print("You have to enter your weight.")
+    }
+
+    
+  }
+  
+  
+  
+  
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    
+    
+    <ScrollView> 
+      <Form onSubmit={calculateBAL}>
+      
+       <View style={Styles.container}>
+      
+         <Text style={Styles.title}>Alcometer</Text>
+      
+      
+         <Text style={Styles.text}>Person's Weight</Text>
+         <TextInput 
+              style={Styles.textInput}
+              keyboardType='numeric'
+              mode='outlined'
+              value={weight}
+              onSubmit={e => setWeight(e.target.value)}
+              maxLength={10}  
+               >
+
+              
+
+          </TextInput>
+
+         <Text style={Styles.text}>Bottles</Text>
+          <View style={Styles.container} >
+          <NumericInput onChange={e => setBottles(e.target.value)} value={bottles}/>
+        </View>
+
+        <Text style={Styles.text}>Hours</Text>
+         <View style={Styles.container}>
+          <NumericInput onChange={e => setTime(e.target.value)} value={time}/>
+        </View>
+      
+  
+
+          <RadioButton.Group onValueChange={e => setRadioval(e.target.value)} value={radioval}>
+            {radios.map(radio =>
+           <View style={radioStyle} key={radio.val.male}>
+              <RadioButton id={male} value={radio.val.male}/>
+            <Text style={Styles.text}>{radio.label}</Text> 
+           </View>
+            )}
+
+            {radios.map(radio =>
+           <View style={radioStyle} key={radio.val.female}>
+              <RadioButton id={female} value={radio.val.female}/>
+            <Text style={Styles.text}>{radio.label}</Text> 
+           </View>
+           
+            )}
+          </RadioButton.Group> 
+
+      
+          <Text style={Styles.result}>{result.toFixed(2)}</Text>
+
+          
+
+      <TouchableOpacity onPress={calculateBAL}>
+        <Text style={Styles.buttonText} mode='outlined'>Calculate</Text> 
+        
+      </TouchableOpacity> 
+
+      
+           
+       
+      
+      
+      <StatusBar style="auto"/>
+     </View>
+    </Form>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/Styles.js
@@ -0,0 +1,93 @@
+import { StyleSheet } from "react-native";
+
+  
+
+
+
+
+
+
+  export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#5ef5f7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+      marginTop: 30,
+      marginBottom: 15,
+      backgroundColor: 'skyblue',
+      flexDirection: 'row',
+    },
+    footer: {
+      marginTop: 20,
+      backgroundColor: 'skyblue',
+      flexDirection: 'row'
+    },
+    title: {
+      color: '#fff',
+      fontWeight: 'bold',
+      flex: 1,
+      fontSize: 23,
+      textAlign: 'center',
+      margin: 10,
+    },
+
+radioStyle: {
+    flexDirection: 'row', 
+    alignItems: 'center',
+},
+    
+title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#120d33',
+    
+},
+    
+text: {
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+},
+    
+textInput:{
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+},
+    
+form:{
+    marginTop: 20,
+    marginBottom: 10,
+    backgroundColor: 'skyblue',
+        
+ },
+    
+buttonText: {
+  borderWidth: 1,
+  borderRadius: 5,
+  textAlign: 'center',
+  fontWeight: 'bold',
+  borderRadius: 6,
+  borderWidth: 3,
+  padding: 9,
+  fontWeight: 'bold'
+
+ },
+
+ result: {
+    flex: 1,
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: 'center',
+
+ }
+ 
+  
+   
+  
+    
+  
+});
